b19a1e0ead85f8e7bff6bd35e9b28d0a1728135e
Manoj Kumar <manojkumar.p@probeseven.com>
2020-03-14 07:54:02 +0530

    code cleanup

--- src/urls.js
+++ src/urls.js
@@ -3,7 +3,7 @@ const env = process.env.API_ENV;
 export const API_URL = (() => {
   switch (env) {
     case "prod":
-      return "https://api.cerebroinc.com";
+      return "http://expresstwitterapi-env.eba-4nfupsux.ap-south-1.elasticbeanstalk.com/api/";
 	  
     default:
       return "http://localhost:8080/api/";

4e7acea9d13e64c1d20a53b94722f31328db0bcc
Manoj Kumar <manojkumar.p@probeseven.com>
2020-03-14 08:52:43 +0530

    API url updates

--- src/urls.js
+++ src/urls.js
@@ -2,10 +2,14 @@ const env = process.env.API_ENV;
 
 export const API_URL = (() => {
   switch (env) {
+    
     case "prod":
       return "http://expresstwitterapi-env.eba-4nfupsux.ap-south-1.elasticbeanstalk.com/api/";
-	  
-    default:
+
+    case "local":
       return "http://localhost:8080/api/";
+      	  
+    default:
+      return "http://expresstwitterapi-env.eba-4nfupsux.ap-south-1.elasticbeanstalk.com/api/";
   }
 })()
